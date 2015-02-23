f63b0b8072ba4762b981bbddff3c8fc75cc0c9a8
sakuya213 <jaroslaw.kotlorz@gmail.com>
2015-02-23 13:32:29 +0100

    Update persontest.php

--- fasada/persontest.php
+++ fasada/persontest.php
@@ -4,24 +4,22 @@ require_once('./class.php');
 
 class PersonTest extends PHPUnit_Framework_TestCase 
 { 
-	private $name;
-	private $surname;
+	private $person;
 
 	public function __construct()
 	{
-	$this->name = new Person('Stefan');
-	$this->surname = new Person('Batory')
+	$this->person = new Person('Stefan','Batory');
 	}
 
     public function testGetName() {
 
-        $Jarek = $this->name->getName();
+        $Jarek = $this->person->getName();
         $this->assertEquals("Stefan",$Jarek);
     }
 
     public function testGetSurname() {
 
-    	$Kotlorz = $this->surname->getSurname();
+    	$Kotlorz = $this->person->getSurname();
     	$this->assertEquals("Batory",$Kotlorz)
     }
 

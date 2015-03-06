37dffbce861acebafdfe8c5ef929066488089207
sakuya213 <jaroslaw.kotlorz@gmail.com>
2015-03-06 22:32:52 +0100

    improvement syntax

--- fasada/secondname.php
+++ fasada/secondname.php
@@ -5,20 +5,23 @@ class SecondName
 
 private $secondname;
 
-	public function getSecondName(){
+	public function getSecondName()
+	{
 
 		return $this->secondname;
 
 	}
 
-	public function setSecondName($secondname){
+	public function setSecondName($secondname)
+	{
 
 
 		$this->secondname = $secondname;
 
 	}
 
-	public function __construct($secondname){
+	public function __construct($secondname)
+	{
 
 		$this->setSecondName($secondname);
 	}
--- fasada/surname.php
+++ fasada/surname.php
@@ -4,19 +4,18 @@ class Surname
 {
 	private $surname;
 
-	public function getSurname() {
-
+	public function getSurname() 
+	{
 		return $this->surname;
-
 	}
 
-	public function setSurname($surname) {
-
+	public function setSurname($surname) 
+	{
 		$this->surname = $surname;
 	}
 
-	public function __construct($surname) {
-
+	public function __construct($surname) 
+	{
 		$this->setSurname($surname);
 	}
 }

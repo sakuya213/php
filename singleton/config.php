60cec897aa0073ab001a34776b45118de0b663b4
sakuya213 <jaroslaw.kotlorz@gmail.com>
2015-03-06 22:25:06 +0100

    aa

--- singleton/config.php
+++ singleton/config.php
@@ -3,30 +3,33 @@
 class Config 
 {
 
-    private static $instance;
+private static $instance;
 
-    private $config = array(
+private $config = array(
         "name"     =>  "Jarek",
         "surname"  =>  "Kotlorz",
         "old"  =>  "21"
         );
  
-    private function __construct() {}
-    private function __clone() {}
+private function __construct() {}
+private function __clone() {}
  
-    public static function getInstance() {
-        if(self::$instance === null) {
+public static function getInstance() 
+{
+    if(self::$instance === null) {
             self::$instance = new Config();
         }
         return self::$instance;
-    }
+}
 
-    public function setName($name) {
+public function setName($name) 
+{
         $this->config["name"] = $name;
-    }
+}
 
-    public function getName() {
+public function getName() 
+{
         return $this->config["name"];
-    }
+}
 }
  
--- singleton/tests/configtest.php
+++ singleton/tests/configtest.php
@@ -4,26 +4,29 @@ include 'config.php';
 class ConfigTest extends PHPUnit_Framework_TestCase 
 {
 
-	private $config;
+private $config;
 
 
-	public function testGetInstance() {
+public function testGetInstance() 
+{
 
 	$config = Config::getInstance();
 
 
-	public function testSetName($name) {
+	public function testSetName($name) 
+	{
 
 		$this->Config["Jarek"] = $name
 
 	}
 
-	public function testGetName() {
+	public function testGetName() 
+	{
 
 		$Jarek = $this->$name->getName();
         $this->assertEquals("Jarek",$Jarek);
 
 	}
 
-	}
+}
 }
